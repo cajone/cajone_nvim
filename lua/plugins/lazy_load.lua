41b315c0b8f4613abc3d962169a4c0f054f2a4a4
cajone <peat@blueyonder.co.uk>
2024-12-08 17:09:06 +0000

    lsp_config working with lua

--- lua/plugins/lazy_load.lua
+++ lua/plugins/lazy_load.lua
@@ -11,6 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 
+
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({})
+require("lazy").setup({
+  { import = "plugins.lsp.lsp_config" }, -- Lsp Config
+})
---
+++ lua/plugins/lsp/lsp_config.lua
@@ -0,0 +1,26 @@
+M = {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- Ctrl-x Ctrl-o
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            -- { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
+      require("lspconfig").lua_ls.setup {
+        diagnostics = { disable = { "missing-fields", "undefined-global" } },
+      }
+    end
+  }
+}
+return M
+
